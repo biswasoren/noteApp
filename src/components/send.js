e74f7874767e7e65aec36dde253c4f5d7473cc6c
biswa1 <biswa@egnify.com>
2019-11-27 20:13:00 +0530

    jenkin files added

--- src/components/send.js
+++ src/components/send.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './send.css'
 import { Form, TextArea, Button, Icon } from 'semantic-ui-react'
 import { socket } from './socket';
